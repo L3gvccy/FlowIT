b3d52bb0a599667b522fbfe530c290b9f7e6c6fe
L3gvccy <oleksandr_iv@knu.ua>
2026-03-04 14:11:55 +0200

    Initialize turborepo

---
+++ apps/server/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import "dotenv/config";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix("api");
+  app.enableCors();
+  app.enableCors({
+    origin: [process.env.ORIGIN],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "Bearer"],
+  });
+
+  await app.listen(process.env.PORT ?? 3001);
+}
+bootstrap();
